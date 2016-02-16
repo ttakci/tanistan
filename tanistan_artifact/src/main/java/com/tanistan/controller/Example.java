7f427f08571ccad82dee46079f1a923b98aed827
ttakci <ttakci@netas.com.tr>
2016-02-16 18:25:47 +0200

    spring mvc
    spring mvc

---
+++ tanistan_artifact/src/main/java/com/tanistan/controller/Example.java
@@ -0,0 +1,17 @@
+package com.tanistan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class Example {
+
+	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
+	public @ResponseBody String greeting(){
+		return "Greeting";
+	}
+	
+}
